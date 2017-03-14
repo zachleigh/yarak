a4ce98b2ce94d919714623b2ff3d7643482efb06
Zach Leigh <zachleigh@fastmail.jp>
2017-03-14 13:51:11 +0000

    Apply fixes from StyleCI

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -3,16 +3,15 @@
  * The app directory is here for testing purposes only.
  * This file bootstraps the Codeception test suite.
  */
-
 use Phalcon\Mvc\Application;
 use Phalcon\DI\FactoryDefault;
 
-$config = include __DIR__ . '/config.php';
+$config = include __DIR__.'/config.php';
 
-include __DIR__ . '/loader.php';
+include __DIR__.'/loader.php';
 
 $di = new FactoryDefault();
 
-include __DIR__ . '/services.php';
+include __DIR__.'/services.php';
 
 return new Application();
