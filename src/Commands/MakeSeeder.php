487b187d399df311ea46bd91328d87c8370c7cbc
Zach Leigh <zachleigh@fastmail.jp>
2017-03-18 06:56:56 +0900

    Documentation fix.

--- src/Commands/MakeSeeder.php
+++ src/Commands/MakeSeeder.php
@@ -4,7 +4,6 @@ namespace Yarak\Commands;
 
 use Yarak\Config\Config;
 use Yarak\DB\Seeders\SeederCreator;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
