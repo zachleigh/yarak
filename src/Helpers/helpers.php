cecf950e54610c50503a2ab16894b1f1b4608275
Zach Leigh <zachleigh@fastmail.jp>
2017-05-04 09:36:36 +0900

    Use dd function from phalcongelist/dd.

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -4,24 +4,6 @@ use Faker\Factory;
 use Phalcon\Debug\Dump;
 use Yarak\DB\Factories\ModelFactory;
 
-if (!function_exists('dd')) {
-    /**
-     * Dump the passed variables and end the script.
-     *
-     * @param  mixed
-     */
-    function dd()
-    {
-        array_map(function ($x) {
-            $string = (new Dump(null, true))->variable($x);
-
-            echo PHP_SAPI == 'cli' ? strip_tags($string).PHP_EOL : $string;
-        }, func_get_args());
-
-        die(1);
-    }
-}
-
 if (!function_exists('factory')) {
     /**
      * Dump the passed variables and end the script.
