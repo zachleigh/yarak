94eee5c33f93365636dcde7366ef87e5777dc3df
Zach Leigh <zachleigh@fastmail.jp>
2017-11-28 05:50:46 +0900

    Remove mysql password.

--- tests/unit/ConfigTest.php
+++ tests/unit/ConfigTest.php
@@ -187,7 +187,7 @@ class ConfigTest extends \Codeception\Test\Unit
         $config->setConfig('database.password');
 
         $this->assertEquals(
-            'password',
+            '',
             $config->toArray()
         );
     }
